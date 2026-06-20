65d7c9c41d9ca22541b390d39a7ee0d2de9f5cff
Jose Pereira Da Silva Filho <jpfilho@eletrobras.com>
2026-06-20 16:10:59 -0300

    chore: atualizar anonKey com a assinatura do JWT_SECRET correto

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -7,7 +7,7 @@ abstract class SupabaseConfig {
   static const String anonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
       '.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc2MjA4NTUzLCJleHAiOjIwOTE1NjE1NTN9'
-      '.Bg2ItVH7x5nHwT79cqWXFeWWVuFlQjY1-o7Bbpve1Ew';
+      '.8zAtbd1xAy_4MQ_Sm5pbkEG209lFwW9jn9aQPay1wSg';
 
   /// Storage bucket for user avatars
   static const String avatarsBucket = 'flowspace-avatars';
